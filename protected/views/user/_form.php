ee718fbd443c02fb6285eb5a07ab9053728105ca
nf02 <liuzhipeng197@163.com>
2013-05-22 14:08:26 +0800

    Óû§

--- protected/views/user/_form.php
+++ protected/views/user/_form.php
@@ -11,32 +11,41 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<!--<p class="note">Fields with <span class="required">*</span> are required.</p>-->
 
-	<?php echo $form->errorSummary($model); ?>
+	<!--?php echo $form->errorSummary($model); ?-->
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->labelEx($model,'用户名'); ?>
 		<?php echo $form->textField($model,'username',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'username'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->labelEx($model,'密码'); ?>
 		<?php echo $form->passwordField($model,'password',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'password'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->labelEx($model,'邮箱'); ?>
 		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'email'); ?>
 	</div>
-
+	
+	<!--
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<!?php echo CHtml::submitButton($model->isNewRecord ? '注册' : 'Save'); ?>
 	</div>
-
+	-->
+	<div>
+		<?php $this->widget('bootstrap.widgets.TbButton',array(
+			'buttonType'=>'submit',
+			'type'=>'primary',
+			'label'=>'注册'
+		));?>
+	</div>	
+	
 <?php $this->endWidget(); ?>
 
 </div><!-- form -->
--- protected/views/user/create.php
+++ protected/views/user/create.php
@@ -1,18 +1,18 @@
 <?php
 /* @var $this UserController */
 /* @var $model User */
-
+/*
 $this->breadcrumbs=array(
 	'Users'=>array('index'),
 	'Create',
 );
-
 $this->menu=array(
 	array('label'=>'List User', 'url'=>array('index')),
 	array('label'=>'Manage User', 'url'=>array('admin')),
 );
+*/
 ?>
 
-<h1>Create User</h1>
+<h3>欢迎加入数据可视化网站</h3>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
