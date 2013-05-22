cdc5da8214440b2d2a439f4345f7dab5adf08421
nf02 <liuzhipeng197@163.com>
2013-05-22 11:58:29 +0800

    Ê×ҳ

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,18 +3,122 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
+<div>
+	<p></p>
+</div>
+<div class='container-fluid'>
+	<div class="row-fluid">
+		<div class="span10">
+			<?php $this->beginWidget('bootstrap.widgets.TbHeroUnit', array(
+    				'heading'=>'数据可视化!',
+			)); ?>
+ 
+    			<p>数据可视化主要旨在借助于图形化手段，清晰有效地传达与沟通信息.</p>
+    			<p><?php $this->widget('bootstrap.widgets.TbButton', array(
+        			'type'=>'primary',
+        			'size'=>'large',
+        			'label'=>'加入我们吧！',
+        			'url'=>array('/site/contact')
+    			)); ?></p>
+ 
+			<?php $this->endWidget(); ?>
+			<li class="span2">
+    				<a href="#" class="thumbnail" rel="tooltip" data-title="twitter实时数据">
+        				<img src="../../images/twitter_1.jpg" alt="" height="25%" width="95%">
+    				</a>
+			</li>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+			<li class="span2">
+    				<a href="#" class="thumbnail" rel="tooltip" data-title="小说标签可视化">
+					<img src="../../images/Plot-lines.jpg" alt="" height="25%" width="90%">
+				</a>
+			</li>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+			<li class="span2">
+    				<a href="#" class="thumbnail" rel="tooltip" data-title="能源与医疗可视化">
+        				<img src="../../images/VizInsight.png" alt="" height="25%" width="100%">
+    				</a>
+			</li>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+			<li class="span2">
+    				<a href="#" class="thumbnail" rel="tooltip" data-title="追踪个人信息">
+					<img src="../../images/bigdata_1.jpg" alt="" height="25%" width="90%">
+    				</a>
+			</li>
+			<li class="span2">
+				<a href="#" class="thumbnail" rel="tooltip" data-title="Ayasdi公司可视化">
+					<img src="../../images/Ayasdl.jpg" alt="" height="100%" width="70%">                                                                                          				      </a>                                                                                                                                                                                          </li>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+		</div>
+		<div class='span2'>
+			<div class="input-append">
+				<input class="span6" id="appendedInputButton" type="text">
+				<button class="btn" type="button">查找</button>
+			</div>	
+			<?php $this->widget('bootstrap.widgets.TbCarousel', array(
+					'items'=>array(
+						array(
+						'image'=>'../../images/dataview_shouye.jpg',
+						'label'=>'First Thumbnail label',
+							),
+      
+						array(
+						'image'=>'../../images/cases.jpg',
+						'label'=>'Second Thumbnail label',
+							),
+      	/*d
+	array(
+		'image'=>'../../images/dataview_shouye.jpg',
+		'label'=>'Third Thumbnail label',
+		'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. ' .
+			'Donec id elit non mi porta gravida at eget metus. ' .
+			'Nullam id dolor id nibh ultricies vehicula ut id elit.'),
+	*/
+  					),
+				));
+			?>
+     		</div>
+	</div>
+</div>
+<?php
+/*
+$this->widget('bootstrap.widgets.TbThumbnails', array(
+	'dataProvider'=>$listDataProvider,
+	'template'=>"{items}\n{pager}",
+	'itemView'=>'_thumb',
+));
+*/
+?>
+<!--
+<li class="span2">
+    <a href="#" class="thumbnail" rel="tooltip" data-title="twitter实时数据">
+	<img src="../../images/twitter_1.jpg" alt="" height="25%" width="95%">
+    </a>
+</li>
+
+<li class="span2">
+    <a href="#" class="thumbnail" rel="tooltip" data-title="小说标签可视化">
+        <img src="../../images/Plot-lines.jpg" alt="" height="25%" width="90%">
+    </a>
+</li>
+
+<li class="span2">
+    <a href="#" class="thumbnail" rel="tooltip" data-title="能源与医疗可视化">
+        <img src="../../images/VizInsight.png" alt="" height="25%" width="100%">
+    </a>
+</li>
+
+                                                                                                                                                                                                      
+<li class="span2">                                                                                                                                                                                    
+    <a href="#" class="thumbnail" rel="tooltip" data-title="追踪个人信息">                                                                                                                        
+        <img src="../../images/bigdata_1.jpg" alt="" height="25%" width="90%">
+    </a>                                                                                                                                                                                              
+</li>
+
+                                                                                                                                                                                                      
+<li class="span2">                                                                                                                                                                                    
+    <a href="#" class="thumbnail" rel="tooltip" data-title="Ayasdi公司可视化">                                                                                                                        
+        <img src="../../images/Ayasdl.jpg" alt="" height="100%" width="70%">                                                                                                                      
+    </a>                                                                                                                                                                                              
+</li>
+-->
